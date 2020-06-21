5a1d7d2a9e0a1fa17801dbe7b0394c65a9a1016f
aleghise <50118159+aleghise@users.noreply.github.com>
2020-06-21 15:51:16 +0200

    Prima Release
    VM Vg

---
+++ Test1/Test1/Form1.Designer.vb
@@ -0,0 +1,227 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PanStatus = New System.Windows.Forms.Panel()
+        Me.LabStatus = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.ProgressBar2 = New System.Windows.Forms.ProgressBar()
+        Me.ProgressBar3 = New System.Windows.Forms.ProgressBar()
+        Me.ProgressBar4 = New System.Windows.Forms.ProgressBar()
+        Me.ProgressBar5 = New System.Windows.Forms.ProgressBar()
+        Me.ProgressBar6 = New System.Windows.Forms.ProgressBar()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.OperationToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DFToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SFTPToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TreeVsftp = New System.Windows.Forms.TreeView()
+        Me.PanStatus.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(4, 56)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 56)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(130, 89)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PanStatus
+        '
+        Me.PanStatus.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PanStatus.Controls.Add(Me.LabStatus)
+        Me.PanStatus.Location = New System.Drawing.Point(11, 428)
+        Me.PanStatus.Name = "PanStatus"
+        Me.PanStatus.Size = New System.Drawing.Size(946, 22)
+        Me.PanStatus.TabIndex = 2
+        '
+        'LabStatus
+        '
+        Me.LabStatus.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LabStatus.AutoSize = True
+        Me.LabStatus.Location = New System.Drawing.Point(0, 0)
+        Me.LabStatus.Margin = New System.Windows.Forms.Padding(1, 0, 1, 0)
+        Me.LabStatus.Name = "LabStatus"
+        Me.LabStatus.Size = New System.Drawing.Size(39, 13)
+        Me.LabStatus.TabIndex = 0
+        Me.LabStatus.Text = "Label1"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(4, 307)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(758, 115)
+        Me.TextBox1.TabIndex = 3
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(4, 131)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(292, 10)
+        Me.ProgressBar1.TabIndex = 4
+        '
+        'ProgressBar2
+        '
+        Me.ProgressBar2.Location = New System.Drawing.Point(3, 160)
+        Me.ProgressBar2.Name = "ProgressBar2"
+        Me.ProgressBar2.Size = New System.Drawing.Size(292, 10)
+        Me.ProgressBar2.TabIndex = 5
+        '
+        'ProgressBar3
+        '
+        Me.ProgressBar3.Location = New System.Drawing.Point(3, 189)
+        Me.ProgressBar3.Name = "ProgressBar3"
+        Me.ProgressBar3.Size = New System.Drawing.Size(292, 10)
+        Me.ProgressBar3.TabIndex = 6
+        '
+        'ProgressBar4
+        '
+        Me.ProgressBar4.Location = New System.Drawing.Point(3, 218)
+        Me.ProgressBar4.Name = "ProgressBar4"
+        Me.ProgressBar4.Size = New System.Drawing.Size(292, 10)
+        Me.ProgressBar4.TabIndex = 7
+        '
+        'ProgressBar5
+        '
+        Me.ProgressBar5.Location = New System.Drawing.Point(3, 247)
+        Me.ProgressBar5.Name = "ProgressBar5"
+        Me.ProgressBar5.Size = New System.Drawing.Size(292, 10)
+        Me.ProgressBar5.TabIndex = 8
+        '
+        'ProgressBar6
+        '
+        Me.ProgressBar6.Location = New System.Drawing.Point(3, 276)
+        Me.ProgressBar6.Name = "ProgressBar6"
+        Me.ProgressBar6.Size = New System.Drawing.Size(292, 10)
+        Me.ProgressBar6.TabIndex = 9
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OperationToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(4, 1, 0, 1)
+        Me.MenuStrip1.Size = New System.Drawing.Size(942, 24)
+        Me.MenuStrip1.TabIndex = 10
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'OperationToolStripMenuItem
+        '
+        Me.OperationToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DFToolStripMenuItem, Me.SFTPToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.OperationToolStripMenuItem.Name = "OperationToolStripMenuItem"
+        Me.OperationToolStripMenuItem.Size = New System.Drawing.Size(72, 22)
+        Me.OperationToolStripMenuItem.Text = "Operation"
+        '
+        'DFToolStripMenuItem
+        '
+        Me.DFToolStripMenuItem.Name = "DFToolStripMenuItem"
+        Me.DFToolStripMenuItem.Size = New System.Drawing.Size(99, 22)
+        Me.DFToolStripMenuItem.Text = "DF"
+        '
+        'SFTPToolStripMenuItem
+        '
+        Me.SFTPToolStripMenuItem.Name = "SFTPToolStripMenuItem"
+        Me.SFTPToolStripMenuItem.Size = New System.Drawing.Size(99, 22)
+        Me.SFTPToolStripMenuItem.Text = "SFTP"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(99, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'TreeVsftp
+        '
+        Me.TreeVsftp.Location = New System.Drawing.Point(585, 44)
+        Me.TreeVsftp.Name = "TreeVsftp"
+        Me.TreeVsftp.Size = New System.Drawing.Size(304, 242)
+        Me.TreeVsftp.TabIndex = 12
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(942, 446)
+        Me.Controls.Add(Me.TreeVsftp)
+        Me.Controls.Add(Me.ProgressBar6)
+        Me.Controls.Add(Me.ProgressBar5)
+        Me.Controls.Add(Me.ProgressBar4)
+        Me.Controls.Add(Me.ProgressBar3)
+        Me.Controls.Add(Me.ProgressBar2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.PanStatus)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Main"
+        Me.Text = "Main"
+        Me.PanStatus.ResumeLayout(False)
+        Me.PanStatus.PerformLayout()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PanStatus As Panel
+    Friend WithEvents LabStatus As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents ProgressBar2 As ProgressBar
+    Friend WithEvents ProgressBar3 As ProgressBar
+    Friend WithEvents ProgressBar4 As ProgressBar
+    Friend WithEvents ProgressBar5 As ProgressBar
+    Friend WithEvents ProgressBar6 As ProgressBar
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents OperationToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DFToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SFTPToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TreeVsftp As TreeView
+End Class
---
+++ Test1/Test1/Form1.vb
@@ -0,0 +1,172 @@
+﻿Imports System.Text.RegularExpressions
+Imports Renci.SshNet
+
+Public Class Main
+
+    Public OpAction As String = "DF"
+    Dim IP As String() = {"10.0.24.96", "10.0.134.75", "10.0.22.27"}
+    Dim Uname As String() = {"aleghise", "root", "root"}
+    Dim Upass As String() = {"alex95", "Password01", "Password01"}
+    Dim remoteDirectory As String = "/"
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListBox1.Items.Add("nagios torino")
+        ListBox1.Items.Add("nagios cr")
+        ListBox1.Items.Add("nagios corsico")
+        ListBox1.SetSelected(0, True)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim result As SshCommand
+
+        TextBox1.Text = ""
+
+        If OpAction = "DF" Then
+            Using ssh = New SshClient(IP(ListBox1.SelectedIndex), Uname(ListBox1.SelectedIndex), Upass(ListBox1.SelectedIndex))
+                ssh.Connect()
+                result = ssh.RunCommand("df -Ph")
+                ssh.Disconnect()
+            End Using
+            Dim lines As String() = result.Result.Split(vbLf)
+            For Each Linea As String In lines
+                TextBox1.Text = TextBox1.Text + vbCrLf + Linea
+            Next
+            ProcessResults(result.Result)
+
+        Else
+            TextBox1.Text = "Works in progress..."
+
+
+            'Dim localDirectory As String = "/"
+
+            TVpopulate(remoteDirectory)
+
+        End If
+
+        LabStatus.Text = OpAction + ": Hai selezionato " + ListBox1.SelectedItem.ToString
+    End Sub
+
+    Private Sub TVpopulate(ByVal LocDir As String)
+        Using sftp = New SftpClient(IP(ListBox1.SelectedIndex), Uname(ListBox1.SelectedIndex), Upass(ListBox1.SelectedIndex))
+            sftp.Connect()
+            Dim files = sftp.ListDirectory(LocDir)
+
+            TreeVsftp.Nodes.Clear()
+
+            For Each Linea In files
+                Dim ParentNode = New TreeNode()
+                ParentNode.Text = Linea.Name
+                TreeVsftp.Nodes.Add(ParentNode)
+                sftp.ChangeDirectory("//" + ParentNode.Text + "/")
+                Dim files2 = sftp.ListDirectory("")
+                For Each S2 In files2
+                    sftp.ChangeDirectory("//" + ParentNode.Text + "/")
+                    Dim ChildtNode = New TreeNode()
+                    ChildtNode.Text = S2.Name
+                    TreeVsftp.Nodes.Add(ChildtNode)
+                    sftp.ChangeDirectory(".")
+                Next
+            Next
+
+            sftp.Disconnect()
+        End Using
+    End Sub
+
+
+    Private Sub TVdir(ByVal LocDir As String)
+        Using sftp = New SftpClient(IP(ListBox1.SelectedIndex), Uname(ListBox1.SelectedIndex), Upass(ListBox1.SelectedIndex))
+            sftp.Connect()
+            Dim files = sftp.ListDirectory(LocDir)
+            sftp.Disconnect()
+
+            TreeVsftp.Nodes.Clear()
+            Dim root = New TreeNode(ListBox1.SelectedItem.ToString)
+            TreeVsftp.Nodes.Add(root)
+            TreeVsftp.Nodes(0).Nodes.Add(New TreeNode("root"))
+            For Each Linea In files
+                Dim remoteFileName As String = Linea.Name
+                'If (Not Linea.Name.StartsWith(".")) AndAlso ((Linea.LastWriteTime.Date = DateTime.Today)) Then
+                If (Not Linea.Name.StartsWith(".")) Then
+                    'TextBox1.Text = TextBox1.Text + vbCrLf + Linea.ToString
+
+                    TextBox1.Text = TextBox1.Text + vbCrLf + Linea.Name + " | " + Linea.Attributes.IsDirectory.ToString + " | " + Linea.Length.ToString
+                    TreeVsftp.Nodes(0).Nodes(0).Nodes.Add(New TreeNode(Linea.Name))
+                End If
+
+            Next
+        End Using
+
+    End Sub
+
+
+    Private Sub ProcessResults(ByVal resultData As String)
+        ProgressBar1.Value = 0
+        ProgressBar2.Value = 0
+        ProgressBar3.Value = 0
+        ProgressBar4.Value = 0
+        ProgressBar5.Value = 0
+        ProgressBar6.Value = 0
+
+        Dim lines As String() = resultData.Split(vbLf)
+        Dim deviceLines As List(Of String) = lines.ToList().Where(Function(x) x.StartsWith("/dev")).ToList()
+        Dim matchPattern As String = "^(/dev/.+)\s+([0-9].+)\s+([0-9].+)\s+([0-9].+)\s+([0-9].+)\s+/.*$"
+
+        For Each deviceLine As String In deviceLines
+            Dim res = Regex.Match(deviceLine, matchPattern)
+            Dim deviceName As String = (res.Groups(1).Value.Replace("/dev/", String.Empty)).Trim()
+            Dim percentUsed As Integer = Convert.ToInt32((res.Groups(5).Value.Replace("%", String.Empty)).Trim())
+
+            Select Case deviceName
+                Case "mapper/vg00-root"
+                    ProgressBar1.Value = percentUsed
+                Case "mapper/VolGroup00-LogVol00"
+                    ProgressBar1.Value = percentUsed
+                Case "sda1"
+                    ProgressBar2.Value = percentUsed
+                Case "sda2"
+                    ProgressBar3.Value = percentUsed
+                Case "sdb1"
+                    ProgressBar4.Value = percentUsed
+                Case "sdb2"
+                    ProgressBar5.Value = percentUsed
+                Case "sdc1"
+                    ProgressBar6.Value = percentUsed
+            End Select
+        Next
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        End
+
+    End Sub
+
+
+
+    Private Sub DFToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DFToolStripMenuItem.Click
+        OpAction = "DF"
+    End Sub
+
+    Private Sub SFTPToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SFTPToolStripMenuItem.Click
+        OpAction = "SFTP"
+    End Sub
+
+    Private Sub OperationToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OperationToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub MenuStrip1_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles MenuStrip1.ItemClicked
+
+    End Sub
+
+    Private Sub TreeVsftp_AfterSelect(sender As Object, e As TreeViewEventArgs) Handles TreeVsftp.AfterSelect
+        remoteDirectory = remoteDirectory + TreeVsftp.SelectedNode.Text + "/"
+        LabStatus.Text = OpAction + ": Hai selezionato " + remoteDirectory
+        TVdir(remoteDirectory)
+    End Sub
+
+
+End Class
